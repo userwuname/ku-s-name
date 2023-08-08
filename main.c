c6101efba3a5acccf95ca8a4dc5b4822ab43a0f8
xiaowus-warehouse <1725272500@qq.com>
2023-08-08 17:48:16 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	scanf("%d,%d", &a, &b);
+	for (a = 0; a < 10;a++) {
+		if (a = 5){
+			printf("l¸ßÎÄ½ÜÀÏÔüÄÐ");
+		}
+	}
+	return 0;
+}
